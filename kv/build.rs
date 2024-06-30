d03311c027cf7c238ab359d6e23ce7822a19bfce
leo233 <leo233.huang@gmail.com>
2024-06-30 18:27:05 +0800

    1. 修复build.rs 2. 实现基础的pb模块

--- kv/build.rs
+++ kv/build.rs
@@ -8,7 +8,9 @@ fn main() {
 
     let mut config = prost_build::Config::new();
     config.bytes(&["."]);
-    config.type_attribute(".", "#[derive(PartialEq)]");
+    // 该配置项会为所有类型添加 PartialOrd trait
+    // 小心如果编译生成的如果自带了重复的属性, 使用它时可能导致编译失败
+    config.type_attribute(".", "#[derive(PartialOrd)]");
     config
         .out_dir("src/pb")
         .compile_protos(&["abi.proto"], &["."])
